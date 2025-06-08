29e63d58611d41664cf0183655e46f66c11a3e48
viole <violetraisondetre@gmai.com>
2025-06-08 13:14:22 +0900

    feat: add helloImagen flow and related prompt
    - Created a new flow for helloImagen in functions/genkit-flows/imagenFlow.ts
    - Added a prompt configuration for helloImagen in functions/prompts/helloImagen.prompt
    - Updated index.ts to include the GenerateImagen callable function
    - Refactored import statements for consistency in firebaseAdmin.ts and index.ts

--- functions/index.ts
+++ functions/index.ts
@@ -1,7 +1,8 @@
-import { onCall, CallableRequest} from 'firebase-functions/v2/https'
+import { onCall, CallableRequest } from 'firebase-functions/v2/https'
 import { googleAIapiKey } from './genkit'
 import { helloGemini } from './genkit-flows/helloGeminiFlow'
 import { verifyAuth } from './firebaseAdmin'
+import { helloImagen } from './genkit-flows/imagenFlow'
 
 const opts = { secrets: [googleAIapiKey], region: `asia-northeast1`, cors: true }
 
@@ -10,5 +11,13 @@ export const helloGenkit = onCall(
   async (request: CallableRequest) => {
     verifyAuth(request);
     return await helloGemini(request.data);
+  }
+);
+
+export const GenerateImagen = onCall(
+  opts,
+  async (request: CallableRequest) => {
+    verifyAuth(request);
+    return await helloImagen(request.data);
   }
 );
