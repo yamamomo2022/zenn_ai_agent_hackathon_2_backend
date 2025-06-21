6286d1ddf81917cee99ca7929c739545b6698403
yamamomo <violetraisondetre@gmail.com>
2025-06-21 16:38:28 +0900

    feat: implement error handling wrapper for callable functions

--- functions/index.ts
+++ functions/index.ts
@@ -4,29 +4,30 @@ import { helloGemini } from './genkit-flows/helloGeminiFlow'
 import { verifyAuth } from './firebaseAdmin'
 import { helloImagen } from './genkit-flows/imagenFlow'
 import { geocoding } from './genkit-flows/geocodingFlow'
+import { withErrorHandler } from './utils'
 
 const opts = { secrets: [googleAIapiKey], region: `asia-northeast1`, cors: true }
 
 export const helloGenkit = onCall(
   opts,
-  async (request: CallableRequest) => {
+  withErrorHandler(async (request: CallableRequest) => {
     verifyAuth(request);
     return await helloGemini(request.data);
-  }
+  })
 );
 
 export const GenerateImagen = onCall(
   opts,
-  async (request: CallableRequest) => {
+  withErrorHandler(async (request: CallableRequest) => {
     verifyAuth(request);
     return await helloImagen(request.data);
-  }
+  })
 );
 
 export const Geocoding = onCall(
   opts,
-  async (request: CallableRequest) => {
+  withErrorHandler(async (request: CallableRequest) => {
     verifyAuth(request);
     return await geocoding(request.data);
-  }
+  })
 );
